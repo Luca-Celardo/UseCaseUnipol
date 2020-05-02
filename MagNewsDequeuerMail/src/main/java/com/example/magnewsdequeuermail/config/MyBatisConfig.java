1a8c2b4c47ef731cf8edad06178f4ff0b34bec89
LucaCelardo <luca.celardo@gmail.com>
2020-05-02 19:17:53 +0200

    Updated project

--- MagNewsDequeuerMail/src/main/java/com/example/magnewsdequeuermail/config/MyBatisConfig.java
+++ MagNewsDequeuerMail/src/main/java/com/example/magnewsdequeuermail/config/MyBatisConfig.java
@@ -4,6 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.example.magnewsdequeurermail.persistence")
+@MapperScan("com.example.magnewsdequeuermail.persistence")
 public class MyBatisConfig {
 }
