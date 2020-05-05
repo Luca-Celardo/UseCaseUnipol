f6326ac0aed04c3940fa10cfa0e5674509d2a5bd
LucaCelardo <luca.celardo@gmail.com>
2020-05-05 11:35:54 +0200

    Updated project

--- MagNewsDequeuerMail/src/main/java/com/example/magnewsdequeuermail/config/KafkaConsumerConfig.java
+++ MagNewsDequeuerMail/src/main/java/com/example/magnewsdequeuermail/config/KafkaConsumerConfig.java
@@ -20,19 +20,19 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, JSONObject> consumerFactory() {
-        JsonDeserializer<JSONObject> deserializer = new JsonDeserializer<>(JSONObject.class);
+        /*JsonDeserializer<JSONObject> deserializer = new JsonDeserializer<>(JSONObject.class);
         deserializer.setRemoveTypeHeaders(false);
         deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeMapperForKey(true);
+        deserializer.setUseTypeMapperForKey(true);*/
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap.kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "magnews-dequeuermail-consumer-group-id");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer());
     }
 
     @Bean
